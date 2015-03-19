fa075fb216283bb9cf93e8d73f245d612153f3fb
David Hsu <dvdhsu@gmail.com>
2015-03-19 08:05:27 -0700

    When GETting a user, guard against user id not found.

--- app/controllers/api_users_controller.rb
+++ app/controllers/api_users_controller.rb
@@ -14,7 +14,7 @@ class ApiUsersController < ApplicationController
 
   # GET /users/1.json
   def show
-    if @user.id != current_user.id
+    if @user.nil? || @user.id != current_user.id
       render json: { status: "failure", error: "User not found, or unauthorized." }
     else
       render json: { status: "success", data: @user.as_json(include: { events: { include: :channels } }) }

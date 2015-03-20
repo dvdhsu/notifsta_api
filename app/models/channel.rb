570402b5ba89c87c7a75cea8e50ea8b71b565345
David Hsu <dvdhsu@gmail.com>
2015-03-19 22:04:47 -0700

    Add validations to Channel.
    Channel must have a name. The name must also be unique, given the Event.

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -3,4 +3,6 @@ class Channel < ActiveRecord::Base
   has_many :notifications, -> { order 'id desc' }, dependent: :destroy
 
   validates :event, presence: true
+  validates :name, presence: true
+  validates_uniqueness_of :name, scope: :event_id
 end

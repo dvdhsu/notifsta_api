db0b3a9467cdcdc308c7ac4801396e721bfe10b0
David Hsu <dvdhsu@gmail.com>
2015-03-19 22:06:20 -0700

    Add validation to Event to require name.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,7 @@
 class Event < ActiveRecord::Base
   has_many :subscriptions, dependent: :destroy
   has_many :users, through: :subscriptions
-
   has_many :channels, dependent: :destroy
+
+  validates :name, presence: true
 end

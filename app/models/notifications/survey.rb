7a54d94bef20c4164d364ffe7719c8363ee81adc
David Hsu <dvdhsu@gmail.com>
2015-03-20 09:48:29 -0700

    Add dependent destroy in Survey for Options.

--- app/models/notifications/survey.rb
+++ app/models/notifications/survey.rb
@@ -1,4 +1,4 @@
 class Survey < Notification
-  has_many :options, foreign_key: "notification_id"
+  has_many :options, foreign_key: "notification_id", dependent: :destroy
   has_many :responses, through: :options
 end

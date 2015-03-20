62442880da14eeeb1fad619992560afa96dfae2b
David Hsu <dvdhsu@gmail.com>
2015-03-19 21:57:08 -0700

    Add `dependent: :destroy' to Channel.
    If a Channel is destroyed, its notifications must be as well.

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -1,6 +1,6 @@
 class Channel < ActiveRecord::Base
   belongs_to :event
-  has_many :notifications, -> { order 'id desc' }
+  has_many :notifications, -> { order 'id desc' }, dependent: :destroy
 
   validates :event, presence: true
 end

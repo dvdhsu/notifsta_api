b29c850ff5e9c9222271f16ba7409db540f8f60a
David Hsu <dvdhsu@gmail.com>
2015-03-19 22:33:54 -0700

    Update Option with validations and `dependent: :destroy'.
    Now, when an Option id destroyed, its responses are as well.
    
    Validations ensure that each Option belongs to a Survey, and that `option_guts'
    is set. Lastly, `option_guts' is unique, given `notification_id', since it
    doesn't make sense to have two options in a survey that is the same.

--- app/models/notifications/option.rb
+++ app/models/notifications/option.rb
@@ -1,4 +1,8 @@
 class Option < ActiveRecord::Base
   belongs_to :survey, foreign_key: "notification_id"
-  has_many :responses
+  has_many :responses, dependent: :destroy
+
+  validates :survey, presence: true
+  validates :option_guts, presence: true
+  validates_uniqueness_of :option_guts, scope: :notification_id
 end

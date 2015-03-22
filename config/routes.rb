94d09ac8547ee7870170663d16f248238282e6dd
David Hsu <dvdhsu@gmail.com>
2015-03-21 21:15:36 -0700

    Remove API namespacing.

--- config/routes.rb
+++ config/routes.rb
@@ -4,25 +4,23 @@ NotifstaWebapp::Application.routes.draw do
   get "home", to: "pages#home", as: "home"
   get "event_admin", to: "pages#event_admin", as: "event_admin"
 
-  scope 'api' do
-    scope 'v1' do
-      scope 'auth' do
-        get 'login' => 'api_authentication#login'
-        get 'logout' => 'api_authentication#logout'
-        get 'get_authentication_token' => 'api_authentication#get_authentication_token'
-      end
-      resources :users, only: [:show], controller: :api_users
-      resources :events, only: [:show], controller: :api_events do
-        resources :channels, only: [:index], controller: :api_channels
-      end
-      resources :channels, only: [:show], controller: :api_channels do
-        resources :notifications, only: [:index, :create], controller: :api_notifications
-      end
-      resources :notifications, only: [:show], controller: :api_notifications do
-        resources :responses, only: [:index, :show, :create], controller: :api_responses
-      end
-      resources :responses, only: [:show], controller: :api_responses
+  scope 'v1' do
+    scope 'auth' do
+      get 'login' => 'api_authentication#login'
+      get 'logout' => 'api_authentication#logout'
+      get 'get_authentication_token' => 'api_authentication#get_authentication_token'
     end
+    resources :users, only: [:show], controller: :api_users
+    resources :events, only: [:show], controller: :api_events do
+      resources :channels, only: [:index], controller: :api_channels
+    end
+    resources :channels, only: [:show], controller: :api_channels do
+      resources :notifications, only: [:index, :create], controller: :api_notifications
+    end
+    resources :notifications, only: [:show], controller: :api_notifications do
+      resources :responses, only: [:index, :show, :create], controller: :api_responses
+    end
+    resources :responses, only: [:show], controller: :api_responses
   end
   
   devise_for :users

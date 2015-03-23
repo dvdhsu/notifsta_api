fc7aa3b58f603a650aa020b0932c7dc46b964458
David Hsu <dvdhsu@gmail.com>
2015-03-22 23:59:25 -0700

    Remove EventsUsers table, since it'd been deprecated by Subscriptions.

--- db/migrate/20150306020158_create_events_users_table.rb
+++
@@ -1,9 +0,0 @@
-class CreateEventsUsersTable < ActiveRecord::Migration
-  def change
-    create_table :events_users do |t|
-      t.integer :event_id
-      t.integer :user_id
-    end
-  add_index :events_users, [:event_id, :user_id]
-  end
-end

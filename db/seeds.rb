6f85da69bac761ffe41235196ef6aa4d375d0e6f
David Hsu <dvdhsu@gmail.com>
2015-03-23 23:06:00 -0700

    Improve seeds file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -36,13 +36,35 @@ food_channel.save!
 logistics_channel.save!
 
 gen_channel.notifications.create!(type: "Message", notification_guts: "first notification in general!")
-gen_channel.notifications.create!(type: "Message", notification_guts: "second notification in general!")
-gen_channel.notifications.create!(type: "Message", notification_guts: "third notification in general!")
 
 food_channel.notifications.create!(type: "Message", notification_guts: "first notification in food!")
-food_channel.notifications.create!(type: "Message", notification_guts: "second notification in food!")
-food_channel.notifications.create!(type: "Message", notification_guts: "third notification in food!")
+survey = food_channel.notifications.create!(type: "Survey", notification_guts: "What food?")
+
+survey.options.create!(option_guts: "Burgers")
+survey.options.create!(option_guts: "Pizza")
 
 logistics_channel.notifications.create!(type: "Message", notification_guts: "first notification in logistics!")
-logistics_channel.notifications.create!(type: "Message", notification_guts: "second notification in logistics!")
-logistics_channel.notifications.create!(type: "Message", notification_guts: "third notification in logistics!")
+
+# subscribe users, and respond to survey
+# first user is admin
+for u in User.all
+ u.subscriptions.create!(event_id: 1)
+ Response.create!(user_id: u.id, option_id: (u.id % 2) + 1)
+end
+
+admin_sub = Subscription.where(user_id: 1).first
+admin_sub.admin = true
+admin_sub.save!
+
+
+
+
+
+
+
+
+
+
+
+
+

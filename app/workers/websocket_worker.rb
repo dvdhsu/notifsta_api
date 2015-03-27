24027a10d5d472cfe9be7abaad37967fb6637c5f
David Hsu <dvdhsu@gmail.com>
2015-03-27 15:57:00 -0800

    Pass more information via Websocket.
    Previously, websocket did not send out Survey options. This fixes that, as well
    as adding some other new fields.

--- app/workers/websocket_worker.rb
+++ app/workers/websocket_worker.rb
@@ -4,14 +4,21 @@ class WebsocketWorker
 
   def perform(notification_id)
     @notification = Notification.find(notification_id)
+    @is_survey = @notification.is_a?(Survey)
+    @options = @is_survey ? @notification.options.select(:id, :option_guts).to_a : nil
+
     data = {
       notification: {
+        id: @notification.id,
+        type: @notification.type,
         notification_guts: @notification.notification_guts,
-        created_at: @notification.created_at
-      },
-      channel_id: @notification.channel.id
+        created_at: @notification.created_at,
+        channel_id: @notification.channel.id,
+        options: @options
+      }
     }
-    puts "notifications_#{@notification.channel.event.name}"
+
+    puts "Sending out websocket notification..."
     WebsocketRails["notifications_#{@notification.channel.event.name}".to_sym].trigger 'new', data
   end
 

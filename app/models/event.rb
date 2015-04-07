f23ae4d258c743f650fcd1abc2f5db02086b78a2
David Hsu <dvdhsu@gmail.com>
2015-04-07 11:19:44 -0700

    Update Event model to add Geocoding and validations.

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,6 +4,12 @@ class Event < ActiveRecord::Base
   has_many :channels, dependent: :destroy
 
   validates :name, presence: true
+  validates :address, presence: true
+  validates :start_time, presence: true
+  validates :end_time, presence: true
+
+  geocoded_by :address
+  after_validation :geocode
 
   def admins
     self.subscriptions.where(admin: true)

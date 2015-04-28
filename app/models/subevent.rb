58e9eabcc361cb52caafcf575f0710847849e7ac
David Hsu <dvdhsu@gmail.com>
2015-04-28 20:55:14 +0100

    Validate start_time is before end_time for Subevent.

--- app/models/subevent.rb
+++ app/models/subevent.rb
@@ -5,4 +5,11 @@ class Subevent < ActiveRecord::Base
   validates :location, presence: true
   validates :start_time, presence: true
   validates :end_time, presence: true
+  validate :start_time_before_end_time
+
+  def start_time_before_end_time
+    if start_time >= end_time
+      errors.add(:start_time, "can't be before end_time")
+    end
+  end
 end

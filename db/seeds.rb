ac565ac15f53f763ea56db59f824f181708ab8c0
David Hsu <dvdhsu@gmail.com>
2015-05-05 16:46:37 +0100

    DB seeds for production.

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,54 +21,41 @@ u = User.new(
 u.skip_confirmation!
 u.save!
 
-# Test user accounts
-(1..50).each do |i|
-  u = User.new(
-      email: "user#{i}@example.com",
-      password: "1234",
-      password_confirmation: "1234"
-  )
-  u.skip_confirmation!
-  u.save!
-
-  puts "#{i} test users created..." if (i % 5 == 0)
-end
-
 hack_london_start_time = DateTime.new(2015, 5, 15, 10, 0 , 0, "+1:00")
-oxford_inspires_start_time = DateTime.new(2015, 5, 18, 10, 0, 0, "+1:00")
+orielball_start_time = DateTime.new(2015, 5, 18, 10, 0, 0, "+1:00")
 hughsball_start_time = DateTime.new(2015, 5, 9, 20, 0, 0, "+1:00")
 
-e1 = Event.create!(name: "hack_london", cover_photo_url: "http://cdn.notifsta.com/images/india.jpg",
-              address: "Strand Campus, King's College London",
-              start_time: hack_london_start_time,
-              end_time: hack_london_start_time.advance(days: 1),
-              description: "Hack London is the largest U.K. hackathon. We hope you enjoy your time here.",
-              facebook_url: "https://www.fb.com/hacklondonuk",
-              website_url: "http://hacklondon.org")
-
-e2 = Event.create!(name: "Oxford Inspires", cover_photo_url: "http://cdn.notifsta.com/images/walking.jpg",
-              address: "Said Business School, Oxford, UK",
-              start_time: oxford_inspires_start_time,
-              end_time: oxford_inspires_start_time.advance(hours: 36),
-              description: "Oxford Inspires hopes to inspire you. Let us know how we can help.",
-              facebook_url: "https://www.fb.com/oxinspires",
-              website_url: "http://oxfordinspires.com")
-
-e3 = Event.create!(name: "St. Hugh's Ball, 2015", cover_photo_url: "https://sthughsball.com/notifsta_background.jpeg",
+e1 = Event.create!(name: "St. Hugh's Ball, 2015", cover_photo_url: "https://sthughsball.com/notifsta_background.jpeg",
               address: "St. Hugh's College, Oxford, UK",
               start_time: hughsball_start_time,
               end_time: hughsball_start_time.advance(hours: 14),
               description: "St. Hugh's is delighted to invite you to our Ball.",
               facebook_url: "https://www.fb.com/sthughsball2015",
-              website_url: "http://sthughsball.com")
+              website_url: "http://sthughsball.com",
+              event_map_url: "http://cdn.notifsta.com/event_maps/hughs.jpg")
+
+e2 = Event.create!(name: "Hack London", cover_photo_url: "http://cdn.notifsta.com/images/walking.jpg",
+              address: "Strand Campus, King's College London",
+              start_time: hack_london_start_time,
+              end_time: hack_london_start_time.advance(days: 1),
+              description: "Hack London is the largest U.K. hackathon. With over 24 hours of hacking, we're sure you'll make something great.",
+              facebook_url: "https://www.fb.com/hacklondonuk",
+              website_url: "http://hacklondon.org")
+
+e3 = Event.create!(name: "Oriel Ball, 2015", cover_photo_url: "https://notifsta.s3.amazonaws.com/bg.jpg",
+              address: "Oriel College, Oxford",
+              start_time: orielball_start_time,
+              end_time: orielball_start_time.advance(days: 1),
+              description: "Oriel Ball 2015 is excited you to welcome you to the Gardens of Babylon.",
+              facebook_url: "https://www.facebook.com/orielball",
+              website_url: "https://orielball.uk")
 
 events = [e1, e2, e3]
 
 for event in events
   start_time = event.start_time
-  end_time = event.end_time
 
-  if event.id == 3
+  if event.id == 1
     event.subevents.create!(name: "Garfunkel", start_time: start_time.advance(minutes: 15),
                             end_time: start_time.advance(hours: 2, minutes: 15), location: "The Enchanted Court")
 
@@ -104,48 +91,16 @@ for event in events
 
     event.subevents.create!(name: "Silent Disco", start_time: start_time.advance(hours: 6, minutes: 15),
                             end_time: start_time.advance(hours: 8), location: "Main Stage (The Glade)")
-  else
-    event.subevents.create!(name: "Welcome event for sponsors", start_time: start_time,
-                            end_time: start_time.advance(minutes: 20), location: "Boardroom", description: "To welcome everybody.")
-    event.subevents.create!(name: "Welcome event for administrators", start_time: start_time,
-                            end_time: start_time.advance(minutes: 20), location: "Gherkin quad", description: "To welcome everybody.")
-
-    event.subevents.create!(name: "Roger Thiel", start_time: end_time.advance(hours: -1),
-                            end_time: end_time.advance(minutes: -30), location: "Lecture Theater 1", description: "To goodbye everybody.")
-    event.subevents.create!(name: "Mark Ramsey", start_time: end_time.advance(hours: -1),
-                            end_time: end_time.advance(minutes: -30), location: "Lecture Theater 2", description: "To goodbye everybody.")
-    event.subevents.create!(name: "Marak Mourash", start_time: end_time.advance(hours: -1), end_time: end_time.advance(minutes: -30),
-                            location: "Lecture Theater 3", description: "To goodbye everybody.")
-
-    event.subevents.create!(name: "Goodbye event", start_time: end_time.advance(minutes: -20), end_time: end_time,
-                            location: "Main quad", description: "To goodbye everybody.")
-    event.subevents.create!(name: "Goodbye event for sponsors", start_time: end_time.advance(minutes: -20), end_time: end_time,
-                            location: "Boardroom", description: "To goodbye everybody.")
-    event.subevents.create!(name: "Goodbye event for administrators", start_time: end_time.advance(minutes: -20), end_time: end_time,
-                            location: "Gherkin quad", description: "To goodbye everybody.")
   end
 
   notifications = event.channels.create!(name: "Notifications")
 
-  if event.id == 1
-    notifications.notifications.create!(type: "Message", notification_guts: "Welcome!")
-    notifications.notifications.create!(type: "Message", notification_guts: "Food now available in the main quad.")
-    notifications.notifications.create!(type: "Message", notification_guts: "Get ready for breakfast!")
-    notifications.notifications.create!(type: "Message", notification_guts: "Breakfast now being served in the Gherkin quad!")
-    notifications.notifications.create!(type: "Message", notification_guts: "Roger Thiel is about to speak in Lecture Theater 1!")
-  elsif event.id == 2
-    notifications.notifications.create!(type: "Message", notification_guts: "Welcome!")
-    notifications.notifications.create!(type: "Message", notification_guts: "Mark Ramsey is about to speak about finance in Lecture Theater 2!")
-    notifications.notifications.create!(type: "Message", notification_guts: "Lunch is now being served in the Main Hall.")
-    notifications.notifications.create!(type: "Message", notification_guts: "Marak Mourash is about to speak about democracies in Lecture Theater 2!")
+  if event.id == 2
+    notifications.notifications.create!(type: "Message", notification_guts: "Welcome to Hack London!")
+  elsif event.id == 3
+    notifications.notifications.create!(type: "Message", notification_guts: "Welcome to the Oriel Commemmoration Ball, 2015. We'll be sending out more notifications closer to the Ball.")
   else
     notifications.notifications.create!(type: "Message", notification_guts: "Welcome to the St. Hugh's Ball, 2015.")
-    notifications.notifications.create!(type: "Message", notification_guts: "Garfunkel will be starting in The Enchanted Court.")
-    notifications.notifications.create!(type: "Message", notification_guts: "Oxford University Jazz Orchestra will be starting on the Main Stage.")
-    notifications.notifications.create!(type: "Message", notification_guts: "Fireworks on the Main Lawn now.")
-    notifications.notifications.create!(type: "Message", notification_guts: "Switch on the DJ Stage, Billie Black in The Enchanted Court, and Jamie Berry on the Main Stage. All in 5 minutes.")
-    notifications.notifications.create!(type: "Message", notification_guts: "Get ready for the Silent Disco on the Main Stage in 5 minutes!")
-    notifications.notifications.create!(type: "Message", notification_guts: "Still around? We've got breakfast at 6.")
   end
 
 =begin

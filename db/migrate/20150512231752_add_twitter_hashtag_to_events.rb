5205767e87821ea3d03b31e0f4acc1bc11ef0ce4
David Hsu <dvdhsu@gmail.com>
2015-05-13 00:19:09 +0100

    Add Twitter Hashtag field to Event.

---
+++ db/migrate/20150512231752_add_twitter_hashtag_to_events.rb
@@ -0,0 +1,5 @@
+class AddTwitterHashtagToEvents < ActiveRecord::Migration
+  def change
+    add_column :events, :twitter_hashtag, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150427001400) do
+ActiveRecord::Schema.define(version: 20150512231752) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20150427001400) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "event_map_url"
+    t.string   "twitter_hashtag"
   end
 
   create_table "friendly_id_slugs", force: :cascade do |t|
